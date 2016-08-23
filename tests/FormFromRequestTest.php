b5ba641facfc454d4fe0ece556679eb0da78b448
Jordi <jordi.nopa@gmail.com>
2016-08-23 21:28:34 +0200

    Preview about testing the fromRequest function

--- tests/FormFromRequestTest.php
+++ tests/FormFromRequestTest.php
@@ -4,11 +4,13 @@ namespace Auburus\FormValidation\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Auburus\FormValidation\Tests\BasicForm;
+use GuzzleHttp\Psr7\Request;
 
 class FormTest extends TestCase
 {
     public function testConstructFromRequest()
     {
+        $request = new Request('GET', 'http://asdf.co');
     }
 
     public function testFromGetRequest()
